8c861e8c6fd4318d6bccdcc65e8fa077d7bb2a8b
John Bradford <jwgbradford@gmail.com>
2021-07-28 13:29:09 +0100

    tidied variable names, tested cumulative growth

--- game_engine.py
+++ game_engine.py
@@ -2,38 +2,52 @@ from local_data_handling import read_data
 
 class Plant():
     def __init__(self, plant_key):
-        self.get_attr(self.get_plant_data(plant_key))
+        self.set_attributes(self.get_plant_data(plant_key))
         self.daily_growth_rate = self.final_height/self.days_to_harvest
+        self.my_height = 0
+
     def get_plant_data(self, plant_key):
         plant_db = read_data('plant.json')
         return plant_db[plant_key]
-    def get_attr(self, plant_model):
+
+    def set_attributes(self, plant_model):
         for key in plant_model:
             setattr(self, key, plant_model[key])
-    def grow(self):
-        print(self.get_growth_from_temp())
-    def get_growth_from_temp(self, growth_from_temp = 0):
+
+    def grow(self, weather_today):
+        temperature_modifier = self.growth_modifier_temperature(weather_today['temp'], weather_today['type'])
+        growth_today = self.daily_growth_rate * temperature_modifier
+        self.my_height += growth_today
+        print(self.my_height)
+
+    def growth_modifier_temperature(self, temp, modifier):
         #protection = plants_modifers[plant_key][todays_weather['type']]
-        if todays_weather['type'] == 'Snow':
+        '''
+        if modifier == 'Snow':
             if self.snow_min_temp != '?':
-                if todays_weather['temp'] < self.snow_min_temp:
+                if temp < self.snow_min_temp:
                     growth_from_temp = 0
             else:
                 growth_from_temp = 0
         else:
-            if (todays_weather['temp'] <= self.max_temp) or (todays_weather['temp'] >= self.min_temp):
-                if todays_weather['temp'] > self.ideal_temp:
-                    growth_from_temp = 1 - ((todays_weather['temp'] - self.ideal_temp) / (self.max_temp - self.ideal_temp))
-                elif todays_weather['temp'] < self.ideal_temp:
-                    growth_from_temp = 1 - ((self.ideal_temp - todays_weather['temp']) / (self.ideal_temp - self.min_temp))
-                else:
-                    growth_from_temp = 1
-            else: 
-                growth_from_temp = 0
+            '''
+        if temp > self.ideal_temp:
+            growth_from_temp = 1 - ((temp - self.ideal_temp) / (self.max_temp - self.ideal_temp))
+        elif temp < self.ideal_temp:
+            if modifier == "Snow":
+                growth_from_temp = 1 - ((self.ideal_temp - temp) / (self.ideal_temp - self.snow_min_temp))
+            else:
+                growth_from_temp = 1 - ((self.ideal_temp - temp) / (self.ideal_temp - self.min_temp))
+        else:
+            growth_from_temp = 1
+        # we don't want our plant to 'ungrow'
+        if growth_from_temp < 0:
+            growth_from_temp = 0
         return growth_from_temp
 
 if __name__ == "__main__":
-    todays_weather, current_plants   = read_data('weather.json')['0'], []
+    weather_history, current_plants = read_data('weather.json'), []
     current_plants.append(Plant(str(input('Do you want a pea plant(0) or a potato plant(1)\n>>> '))))
-    for plant in current_plants:
-        plant.grow()
+    for today in weather_history:
+        for plant in current_plants:
+            plant.grow(weather_history[today])
