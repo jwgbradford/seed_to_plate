5f4a1cb93ce6a5b57341d8c2ce6aa16759a13e56
Thomas <thomas-2415@digilocal.org.uk>
2021-07-29 12:52:54 +0100

    recovery days working

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # Development project 
 
 Building basic game engine and data files for BSF project
-
-## Things to think about
-
-- what if temp goes above maximum temp this code dose not fix this
-```
-if growth_from_temp < 0:
-            growth_from_temp = 0
-```
-- should a day be a day be added for each growth modifier like at the moment or only one each time it appears even if there is multiple
-- if one of the growth modifiers is 0 you waon't grow that day
--- game_engine.py
+++ game_engine.py
@@ -4,7 +4,8 @@ class Plant():
     def __init__(self, plant_key):
         self.set_attributes(self.get_plant_data(plant_key))
         self.daily_growth_rate = self.final_height/self.days_to_harvest
-        self.my_height, self.recovery_days = 0, 0
+        self.my_height, self.stored_water = 0, 0
+        self.sun_recovery_day, self.water_recovery_day, self.temp_recovery_day = 0, 0, 0
 
     def get_plant_data(self, plant_key):
         plant_db = read_data('plant.json')
@@ -15,13 +16,11 @@ class Plant():
             setattr(self, key, plant_model[key])
 
     def grow(self, weather_today):
-        if self.recovery_days <= 0:
-            hydration_modifier = self.growth_modifier_hydration(weather_today['rainfall'])
-            temperature_modifier = self.growth_modifier_temperature(weather_today['temp'], weather_today['type'])
-            growth_today = self.daily_growth_rate * temperature_modifier * hydration_modifier
-            self.my_height += growth_today
-        else:
-            self.recovery_days -= 1
+        sunlight_modifier = self.growth_modifier_sunlight(weather_today['sun'])
+        hydration_modifier = self.growth_modifier_hydration(weather_today['rainfall'])
+        temperature_modifier = self.growth_modifier_temperature(weather_today['temp'], weather_today['type'])
+        growth_today = self.daily_growth_rate * temperature_modifier * hydration_modifier * sunlight_modifier
+        self.my_height += growth_today
 
     def growth_modifier_temperature(self, temp, modifier):
         if temp > self.ideal_temp:
@@ -31,24 +30,48 @@ class Plant():
                 growth_from_temp = 1 - (self.ideal_temp - temp) / (self.ideal_temp - self.snow_min_temp)
             else:
                 growth_from_temp = 1 - (self.ideal_temp - temp) / (self.ideal_temp - self.min_temp)
-        else:
-            growth_from_temp = 1
-        # we don't want our plant to 'ungrow'
-        if growth_from_temp < 0:
+        if (self.temp_recovery_day > 0) and (growth_from_temp > 0):
+            self.temp_recovery_day -= 1
             growth_from_temp = 0
-            self.recovery_days += 1
+        elif growth_from_temp < 0:
+            growth_from_temp = 0
+            self.temp_recovery_day += 1
         return growth_from_temp
 
     def growth_modifier_hydration(self, rainfall):
-        if rainfall != self.ideal_water:
-            growth_from_rainfall = 1 - (self.ideal_water - rainfall) / (self.ideal_water)
+        self.stored_water += rainfall # put rainfall in soil
+        growth_from_hydration = 0 
+        # if there is more water in the soil than too 2 weeks worth of the plan't water consumption
+        # or if there is no water in the soil
+        if (self.stored_water > 14 * self.ideal_water) or (self.stored_water == 0):
+            self.water_recovery_day += 1 
+        # if we would grow but the plant need to recover take of a day to recover and don't grow
+        elif (self.water_recovery_day > 0) and (self.stored_water > self.ideal_water):
+            self.water_recovery_day -= 1
+            self.stored_water -= self.ideal_water
+        # if we would grow and we don't need to recover see how much we would grow from the water
+        elif self.water_recovery_day == 0:
+            if self.stored_water < self.ideal_water:
+                growth_from_hydration = 1-(self.ideal_water - rainfall) / self.ideal_water
+                self.stored_water -= rainfall
+            else:
+                growth_from_hydration = 1
+                self.stored_water -= self.ideal_water
+        return growth_from_hydration
+
+    def growth_modifier_sunlight(self, sunlight):
+        #get how much plant grows
+        if sunlight == self.ideal_sunlight: 
+            growth_from_sunlight = 1
         else:
-            growth_from_rainfall = 1
-        # we don't want our plant to 'ungrow'
-        if growth_from_rainfall < 0:
-            growth_from_rainfall = 0
-            self.recovery_days += 1
-        return growth_from_rainfall
+            growth_from_sunlight = 1 - (self.ideal_sunlight - sunlight) / (self.ideal_sunlight)
+        if (growth_from_sunlight > 0) and (self.sun_recovery_day > 0): # if plant not burning but need to recover
+            self.sun_recovery_day -=1
+            growth_from_sunlight = 0
+        elif growth_from_sunlight < 0: #if plant burnig
+            growth_from_sunlight = 0
+            self.sun_recovery_day += 1
+        return growth_from_sunlight
 
 if __name__ == "__main__":
     weather_history, current_plants = read_data('weather.json'), []
@@ -56,5 +79,5 @@ if __name__ == "__main__":
     for today in weather_history:
         for plant in current_plants:
             plant.grow(weather_history[today])
+            print(plant.sun_recovery_day, plant.water_recovery_day, plant.temp_recovery_day)
             print(plant.my_height)
-            print('rd: ', plant.recovery_days)
