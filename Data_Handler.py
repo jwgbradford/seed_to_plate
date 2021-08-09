9d2ea060978ca9681c3c49e8bbe2605959201a49
John Bradford <jwgbradford@gmail.com>
2021-08-09 18:38:30 +0100

    added second plant to Fruit for testing

--- Data_Handler.py
+++ Data_Handler.py
@@ -1,8 +1,52 @@
 import json
 
 plant_dict = {
-    'Fruit': {0: {'name': 'pea', 'snow_min_temp': -15, 'min_temp': -2, 'ideal_temp': 10, 'max_temp': 21, 'ideal_sunlight': 5, 'ideal_branches': 74.8, 'final_height': 0.9114, 'days_to_flower': 91, 'days_to_fruit': 94, 'days_to_harvest': 142, 'ideal_water': 0.114}},
-    'Tuber': {0: {'name': 'potato', 'snow_min_temp': '?', 'min_temp': 10, 'ideal_temp': 20, 'max_temp': 35, 'ideal_sunlight': 8, 'ideal_branches': 7, 'final_height': 1,'days_to_flower': 50, 'days_to_fruit': 120, 'days_to_harvest': 75,  'ideal_water': 0.1311}}
+    'Fruit': {
+        0: {
+            'name': 'pea', 
+            'snow_min_temp': -15, 
+            'min_temp': -2, 
+            'ideal_temp': 10, 
+            'max_temp': 21, 
+            'ideal_sunlight': 5, 
+            'ideal_branches': 74.8, 
+            'final_height': 0.9114, 
+            'days_to_flower': 91, 
+            'days_to_fruit': 94, 
+            'days_to_harvest': 142, 
+            'ideal_water': 0.114
+        },
+        1: {
+            'name': 'tomato', 
+            'snow_min_temp': '?', 
+            'min_temp': 16, 
+            'ideal_temp': 21, 
+            'max_temp': 27, 
+            'ideal_sunlight': 8, 
+            'ideal_branches': 100, 
+            'final_height': 1.8, 
+            'days_to_flower': 45, 
+            'days_to_fruit': 80, 
+            'days_to_harvest': 120, 
+            'ideal_water': 0.25
+        }
+    },
+    'Tuber': {
+        0: {
+            'name': 'potato', 
+            'snow_min_temp': '?', 
+            'min_temp': 10, 
+            'ideal_temp': 20, 
+            'max_temp': 35, 
+            'ideal_sunlight': 8, 
+            'ideal_branches': 7, 
+            'final_height': 1,
+            'days_to_flower': 50, 
+            'days_to_fruit': 120, 
+            'days_to_harvest': 75,  
+            'ideal_water': 0.1311
+        }
+    }
 }
 
 def write_data(dict_to_save, file_name):
